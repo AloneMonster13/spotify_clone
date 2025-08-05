a74b5ab492839dd9092e461b561454c49e5bc87f
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-05 16:36:41 +0530

    db updated

--- db.php
+++ db.php
@@ -1,9 +1,16 @@
 <?php
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $host = 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'spotify_clone';
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+try {
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    $conn->set_charset('utf8mb4');
+} catch (mysqli_sql_exception $e) {
+    die("Connection failed: " . $e->getMessage());
 }
